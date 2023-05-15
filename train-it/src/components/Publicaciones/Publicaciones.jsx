cf67616a1cec03ffeb3e90912b2117ad6507f7bc
alexander <aloj@fp.insjoaquimmir.cat>
2023-05-15 23:04:57 +0200

    Publicaciones fintradas por categoria

--- train-it/src/components/Publicaciones/Publicaciones.jsx
+++ train-it/src/components/Publicaciones/Publicaciones.jsx
@@ -5,7 +5,36 @@ import './Publicaciones.css'
 
 const Publicaciones = () => {
     const [publicaciones, setPublicaciones] = useState([]);
+    const { categoria } = useParams();
+    const [categoriaId, setCategoriaId] = useState(null);
 
+    useEffect(() => {
+      const fetchCategoria = async () => {
+        try {
+          const response = await axios.get('http://127.0.0.1:8000/api/categorias');
+          const categorias = response.data;
+  
+          // Busca la categoría que coincide con la URL
+          const categoriaEncontrada = categorias.find(cat => cat.nombre === categoria);
+          if (categoriaEncontrada) {
+            // Guarda el ID de la categoría que coincide
+            setCategoriaId(categoriaEncontrada.id);
+          } else {
+            console.log('Categoría no encontrada');
+          }
+        } catch (error) {
+          console.error('Hubo un error al obtener las categorías', error);
+        }
+      };
+      fetchCategoria();
+    }, [categoria]);  // Dependencia en 'categoria' para que se ejecute de nuevo si cambia
+    // console.log(categoriaId);
+    // console.log(categoriaId);
+    // console.log(categoriaId);
+    // console.log(categoriaId);
+    // console.log(categoriaId);
+
+    
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get('http://127.0.0.1:8000/api/posts');
@@ -24,7 +53,7 @@ const Publicaciones = () => {
 
     return (
         <div>
-          {publicaciones.map((publicacion) => (
+          {publicaciones.filter(publicacion => publicacion.categoria_id === categoriaId).map((publicacion) => (
             <div className='Caja_publicacion' key={publicacion.id}>
               <p>User ID: {publicacion.user_id}</p>
               <p>Categoria ID: {publicacion.categoria_id}</p>
