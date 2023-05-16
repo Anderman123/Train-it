b00e495e3a445263dfefa1366882ad48d9076288
alexander <aloj@fp.insjoaquimmir.cat>
2023-05-16 20:40:30 +0200

    Se ha podido mostrar de quien es la publicacion

--- train-it/src/components/Publicaciones/Publicaciones.jsx
+++ train-it/src/components/Publicaciones/Publicaciones.jsx
@@ -5,8 +5,26 @@ import './Publicaciones.css'
 
 const Publicaciones = () => {
     const [publicaciones, setPublicaciones] = useState([]);
-    const { categoria } = useParams();
     const [categoriaId, setCategoriaId] = useState(null);
+    const [usuarios, setUsuarios] = useState([]);
+    const { categoria } = useParams();
+
+
+    useEffect(() => {
+      const fetchUsuario = async () => {
+        try {
+          const response = await axios.get('http://127.0.0.1:8000/api/usuario');
+          // Aquí estamos estableciendo el primer usuario de la lista.
+          setUsuarios(response.data.data);
+        } catch (error) {
+          console.error('Hubo un error al obtener los datos del usuario', error);
+        }
+      };
+      fetchUsuario();
+    }, []);
+    // console.log(usuario.id);
+    // console.log(usuario.name);
+    // console.log(usuarios);
 
     useEffect(() => {
       const fetchCategoria = async () => {
@@ -31,8 +49,7 @@ const Publicaciones = () => {
     // console.log(categoriaId);
     // console.log(categoriaId);
     // console.log(categoriaId);
-    // console.log(categoriaId);
-    // console.log(categoriaId);
+
 
     
     useEffect(() => {
@@ -52,18 +69,30 @@ const Publicaciones = () => {
 
 
     return (
-        <div>
-          {publicaciones.filter(publicacion => publicacion.categoria_id === categoriaId).map((publicacion) => (
+      <div>
+        {/* <div className='Caja_publicacion'>
+            {usuarios.map(usuario => (
+              <p key={usuario.id}>{usuario.id}: {usuario.name}</p>
+            ))}
+        </div> */}
+        {publicaciones.filter(publicacion => publicacion.categoria_id === categoriaId).map((publicacion) => {
+          // Busca al usuario con el mismo id que publicacion.user_id
+          const usuarioPublicacion = usuarios.find(usuario => usuario.id === publicacion.user_id);
+  
+          return (
             <div className='Caja_publicacion' key={publicacion.id}>
               <p>User ID: {publicacion.user_id}</p>
+              {/* Si se encontró el usuario, muestra su nombre */}
+              {usuarioPublicacion && <p>Nombre del usuario: {usuarioPublicacion.name}</p>}
               <p>Categoria ID: {publicacion.categoria_id}</p>
               <p>Descripcion: {publicacion.descripcion}</p>
               {publicacion.imagen && <img src={publicacion.imagen} alt="Imagen de publicación" />}
               {publicacion.video && <video src={publicacion.video} controls />}
             </div>
-          ))}
-        </div>
-      );
-    };
+          );
+        })}
+      </div>
+    );
+  };
     
-    export default Publicaciones;
+export default Publicaciones;
